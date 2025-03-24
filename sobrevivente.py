7ef5887c841aec758b918f7063016c6762c653d9
PedroBlue13 <pedroblue@outlook.com.br>
2025-03-24 17:17:38 -0300

    josephus challenge exercice

--- sobrevivente.py
+++ sobrevivente.py
@@ -1,3 +1,22 @@
+"""
+3 - Josephus
+Um grupo de homens foram capturados pelo exército inimigo, e foram obrigados a se posicionarem em círculo. Onde o primeiro homem deveria atirar no segundo, o terceiro no quarto o quinto no sexto e assim por diante, até que não sobre mais ninguém e uma única pessoa sobreviva.
+Na imagem acima, apenas o homem 3 sobreviveu. Para que você tenha mais chance de sobreviver crie um programa que receba a quantidade de pessoas no círculo e retorno qual será a pessoa sobrevivente.
+Entradas
+Um número inteiro que indica a quantidade de pessoas no círculo.
+Saída
+Um número inteiro que indica o homem sobrevivente
+Exemplo de entradas
+10
+13
+5
+Exemplo de saídas
+5
+11
+3
+"""
+
+
 def josephus(n):
     pessoas = list(range(1, n + 1))
     indice = 1
